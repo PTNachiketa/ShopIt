ff5c28a9f454fb76d6d307b767a04582d2762d49
P Tejaditya Nachiketa <tejadityanachiketa093@gmail.com>
2023-04-24 22:44:35 +0530

    backend online

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ app.use(helmet())
 
 //middleware to allow requests from a domain(CORS)
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://shop-2qzt58rs5-ptnachiketa.vercel.app'); 
+    res.setHeader('Access-Control-Allow-Origin', '*'); 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
