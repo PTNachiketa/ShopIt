5c64951792f01defeb84f3799a3476f8cb9bd654
P Tejaditya Nachiketa <tejadityanachiketa093@gmail.com>
2023-04-27 17:33:24 +0530

    secret added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,7 +2,7 @@ const User = require('../models/userModel')
 const jwt = require('jsonwebtoken')
 
 const createToken = (_id)=>{
-    return jwt.sign({_id},sunflowerblue,{expiresIn:'1d'})
+    return jwt.sign({_id},"sunflowerblue",{expiresIn:'1d'})
 }
 
 // login a user
